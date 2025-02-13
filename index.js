d7273617c0206d97f4b806f17d94499465c1fdfe
Koushil C <koushil463@gmail.com>
2025-02-13 18:05:22 +0530

    changed port number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 
 app.use(express.json())
 
-app.get("/kkr",(req,res)=>{
+app.get("/",(req,res)=>{
     return res.status(201).json({admin:"this is a aws vm launched by koushil"})
 })
 
